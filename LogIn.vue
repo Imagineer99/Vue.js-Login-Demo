698e36d04a8f862ad16c2ac763d21aaa101da669
Lee Jackson <130007945+Imagineer99@users.noreply.github.com>
2023-11-02 21:19:29 +0000

    Update LogIn.vue

--- LogIn.vue
+++ LogIn.vue
@@ -5,12 +5,15 @@
       <transition name="fade">
         <div v-if="!loggedIn" class="login-form">
           <input v-model="username" placeholder="Username">
-          <input v-model="password" type="password" placeholder="Password"> <!-- Password input -->
+          <input v-model="password" type="password" placeholder="Password">
           <button @click="login">Log In</button>
         </div>
       </transition>
       <transition name="fade">
-        <h1 v-if="loggedIn" class="welcome-message">Welcome, {{ username }}</h1>
+        <div v-if="loggedIn" class="welcome-message">
+          <h1>Welcome, {{ username }}</h1>
+          <button @click="logout">Log Out</button>
+        </div>
       </transition>
     </div>
   </div>
@@ -21,7 +24,7 @@ export default {
   data() {
     return {
       username: '',
-      password: '', // Added password data property
+      password: '',
       loggedIn: false,
     };
   },
@@ -32,79 +35,195 @@ export default {
         this.loggedIn = true;
       }
     },
+    logout() {
+      this.loggedIn = false;
+      this.username = '';
+      this.password = '';
+    },
   },
 };
 </script>
 
-
 <style>
+
+:root {
+  --primary-color: #0073e6;
+  --bg-color: #f0f0f0;
+
+  --input-bg: #fff;
+  --input-border: #ccc;
+  --input-hover: #f2f2f2;
+
+  --btn-bg: var(--primary-color);
+  --btn-color: #fff;
+  --btn-hover: #0056b3;
+}
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+  background-color: var(--bg-color);
+}
+
 .login-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  
-  height: 100vh;
-  background-color: #f0f0f0;
+  min-height: 100vh;
 }
-.login-form {
-    margin-top: 0px;
-    display: inline-grid;
-}
+
 .login-box {
-  text-align: center;
+  max-width: 400px;
+  height: 300px;
   padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: #fff;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  text-align: center;
+  border: 1px solid var(--input-border);
+  border-radius: 8px;
+  background-color: var(--input-bg);
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
 }
 
 .header {
-  font-size: 24px;
-  color: #333;
+  font-size: 1.5rem;
+  font-weight: 600; 
+  margin-bottom: 1rem;
+  color: var(--primary-color);
 }
 
 .login-form {
-  margin-top: 0px;
-  
+  display: flex;
+  flex-direction: column;
 }
-input, button {
-  padding: 8px;
-  margin: 4px;
-  border: none;
+
+input,
+button {
+  padding: 0.75rem 1rem;
+  border-radius: 4px; 
 }
 
 input {
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  margin-bottom: 1rem;
+  border: 1px solid var(--input-border);
+  background-color: #fff;
+  transition: border-color 0.2s, background-color 0.2s;
+}
+
+input:focus {
+  border-color: var(--primary-color);
+  background-color: var(--input-hover);
+  outline: none;
+}
+
+input::placeholder {
+  color: #aaa;
 }
 
 button {
-  background-color: #0073e6;
-  color: white;
+  width: 100%;
   border: none;
-  border-radius: 4px;
-  cursor: pointer;
+  background-color: var(--btn-bg);
+  color: var(--btn-color);
+  transition: background-color 0.2s;
+}
+
+button:hover {
+  background-color: var(--btn-hover);
+}
+
+
+
+button:hover::after {
+  margin-left: 1rem; 
 }
 
 .welcome-message {
-  font-size: 20px;
-  margin-top: 20px;
-  color: #0073e6;
+  font-size: 1.25rem;
+  margin-top: 1rem;
+  color: var(--primary-color);
+}
+.login-box {
+    max-width: 400px;
+    height: 300px;
+    padding: 9px !important; 
+    overflow: hidden;
+    text-align: center;
+    border: 1px solid var(--input-border);
+    border-radius: 8px;
+    background-color: var(--input-bg);
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+}
+/* Animations */
+
+@keyframes slideIn {
+  0% {
+    max-height: 0;
+  }
+  
+  100% {
+    max-height: 300px;
+  }
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.66s;
+.fade-enter-active {
+  animation: slideIn 0.5s cubic-bezier(0.645, 0.045, 0.355, 1);  
 }
 
-.fade-enter, .fade-leave-to {
-  opacity: 0;
+@keyframes slideOut {
+  0% {
+    max-height: 300px;
+  }
+
+  100% {
+    max-height: 0;
+  }
+}
+
+.fade-leave-active {
+  animation: slideOut 0.8s cubic-bezier(0.645, 0.045, 0.355, 1);
+}
+/* Animations */
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+
+.fade-enter-active {
+  animation: fadeIn 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+}
+
+.fade-leave-active {
+  animation: fadeOut 0.3s cubic-bezier(0.645, 0.045, 0.355, 1); 
+}
+/* Responsive */
+
+@media (max-width: 576px) {
+  .login-box {
+    padding: 1.5rem; 
+  }
+
+  input,
+  button {
+    width: 100%;
+  }
 }
 </style>
-
-
-<!--
-  WARNING: This code is a basic example for educational and prototyping purposes.
-  It does not include real authentication logic and should not be used in production without proper security measures.
--->
-
